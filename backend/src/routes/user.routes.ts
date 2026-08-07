ad80ff5a557a5af11ff519f61400ba6f34464bb3
Aditya Kumar Soni <soniadityakumar651@gmail.com>
2026-08-08 02:37:18 +0530

    feat: added the backend auth with clerkuser, and postgres db

---
+++ backend/src/routes/user.routes.ts
@@ -0,0 +1,39 @@
+// in this file we are generating routes for the users who are authenticated and want to perform some tasks like patch, edit , many more ->
+import { Router } from 'express'
+import { z } from 'zod'
+import { getAuth } from '../config/clerk'
+import { UnauthorisedError } from '../lib/errors'
+import { getUserfromClerk } from '../modules/users/user.service'
+import {
+  toUserProfileResponse,
+  type UserProfile,
+  type UserProfileResponse,
+} from '../modules/users/user.types'
+export const userRouter = Router()
+
+// in the patch route the user should be able to update the bio, name, handle and avatar url.
+// user update schema =>
+const userProfileUpdateSchema = z.object({
+  displayName: z.string().trim().max(50).optional(),
+  handle: z.string().trim().max(50).optional(),
+  bio: z.string().trim().max(500).optional(),
+  avatarUrl: z.url('Avatar must be a valid url').optional(),
+})
+// now getting the types of the user profile from the user.profile.ts
+function toResponse(profile: UserProfile): UserProfileResponse {
+  return toUserProfileResponse(profile)
+}
+
+userRouter.get('/', async (req, res, next) => {
+  try {
+    const auth = getAuth(req)
+    if (!auth.userId) {
+      throw new UnauthorisedError('UnauthorisedError')
+    }
+    const profile = await getUserfromClerk(auth.userId)
+    const response = toResponse(profile)
+    res.json({ data: response })
+  } catch (error) {
+    console.error(`Error: ${error}`)
+  }
+})
